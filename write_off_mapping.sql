c710e07d94d62ea6d73eda7bb2720f5e9504e388
unknown <wahyuros@PB90048.telkomsel.co.id>
2017-07-26 14:59:01 +0700

    ,working on write_off_mapping query

---
+++ write_off_mapping.sql
@@ -0,0 +1,71 @@
+SET @wo_month = 5;
+
+CREATE
+TEMPORARY TABLE far_not_written_off
+(INDEX (asset_number), UNIQUE (asset_id))
+AS (
+	SELECT * FROM far
+	WHERE write_off_date IS NULL
+);
+
+CREATE
+TEMPORARY TABLE current_write_off
+(INDEX (asset_number), UNIQUE (write_off_id))
+AS (
+	SELECT * FROM write_off
+	WHERE MONTH(give_up_date) = @wo_month
+	GROUP BY asset_number);
+
+CREATE TEMPORARY TABLE far_inner_join_write_off
+(INDEX (asset_number), UNIQUE(asset_id))
+AS (
+	SELECT	far.asset_id AS asset_id,
+		far.asset_number AS asset_number,
+		far.addition_period AS addition_period,
+		far.source AS source,
+		far.category AS category,
+		far.category_id AS category_id,
+		far.cost AS cost
+	FROM far_not_written_off far
+	INNER JOIN current_write_off wo ON wo.asset_number = far.asset_number
+);
+
+
+SET @dynamic_sql = (
+	SELECT
+		GROUP_CONCAT(DISTINCT
+			CONCAT(
+				'sum( if (source = '
+				,'"'
+				, source
+				,'"'
+				,' , cost,0) ) AS '
+				, source,
+				' '
+			))
+	FROM far_not_written_off
+);
+SET @pivot_table_name = "far_far_inner_join_write_off";
+SET @sql = CONCAT(
+		'CREATE TEMPORARY TABLE ',
+		@pivot_table_name,
+		' AS ',
+		'SELECT asset_number, ',
+		@dynamic_sql,
+		'FROM far_inner_join_write_off
+		GROUP BY asset_number'
+		);
+PREPARE pivot FROM @sql;
+EXECUTE pivot;
+DEALLOCATE PREPARE pivot;
+
+CREATE TEMPORARY TABLE mapping_write_off
+AS	
+	SELECT wo.* ,far.* 
+	FROM current_write_off wo
+	LEFT JOIN far_far_inner_join_write_off far ON wo.asset_number = far.asset_number;
+
+
+SHOW INDEX FROM far_inner_join_write_off;
+SHOW STATUS LIKE '%tmp%';
+SELECT * FROM pivot_far_inner_join_write_off;	
